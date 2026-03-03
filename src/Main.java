a9b52a3808c7ce0cc1ffd0b6528339b88bbc24ec
mariitoo006 <pizarromario276@gmail.com>
2026-03-03 17:58:52 +0100

    mensaje2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main (String [] args){
 	System.out.println("Hola Mundo");
+	System.out.println("Segundo Mensaje");
+	System.out.println("Tercer Mensaje");
 	}
 }
